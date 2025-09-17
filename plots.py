5a2352b4d76a98628f93dd19a5dec8bc3ee377e9
Veronica Carrasco <veronica@morta.io>
2025-09-17 20:30:56 +0100

    Copilot comments

--- plots.py
+++ plots.py
@@ -33,25 +33,6 @@ def plot_boxplots(df: pd.DataFrame, outdir: str, show: bool = False):
         plt.close()
 
 
-def plot_lines(df: pd.DataFrame, outdir: str, index_col: Optional[str], show: bool = False):
-    # Si se especifica index_col y existe, úsala como índice (ideal para series temporales)
-    if index_col and index_col in df.columns:
-        df_plot = df.set_index(index_col)
-    else:
-        df_plot = df
-
-    plt.figure()
-    df_plot.plot(title="Serie(s) temporal(es) / Line Plot")
-    plt.xlabel("Índice" if not index_col else index_col)
-    plt.ylabel("Valor")
-    fname = os.path.join(outdir, "line_plot.png")
-    plt.tight_layout()
-    plt.savefig(fname, dpi=150)
-    if show:
-        plt.show()
-    plt.close()
-
-
 def plot_scatter_matrix(df: pd.DataFrame, outdir: str, show: bool = False):
     axarr = scatter_matrix(df, alpha=0.7, diagonal="hist", figsize=(8, 8))
     # Ajuste de etiquetas
@@ -61,23 +42,6 @@ def plot_scatter_matrix(df: pd.DataFrame, outdir: str, show: bool = False):
         ax.yaxis.label.set_ha("right")
     plt.suptitle("Matriz de dispersión", y=1.02)
     fname = os.path.join(outdir, "scatter_matrix.png")
-    plt.tight_layout()
-    plt.savefig(fname, dpi=150)
-    if show:
-        plt.show()
-    plt.close()
-
-
-def plot_corr_heatmap(df: pd.DataFrame, outdir: str, show: bool = False):
-    corr = df.corr(numeric_only=True)
-    plt.figure(figsize=(6, 5))
-    im = plt.imshow(corr, interpolation="nearest")
-    plt.title("Mapa de calor de correlaciones")
-    plt.colorbar(im, fraction=0.046, pad=0.04)
-    ticks = range(len(corr.columns))
-    plt.xticks(ticks, corr.columns, rotation=45, ha="right")
-    plt.yticks(ticks, corr.columns)
-    fname = os.path.join(outdir, "correlation_heatmap.png")
     plt.tight_layout()
     plt.savefig(fname, dpi=150)
     if show:
