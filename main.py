2371f690813b39a6ff1d928fb1403cd674ec4903
Veronica Carrasco <veronica@morta.io>
2025-09-17 20:22:22 +0100

    Refactored code

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import os
-from estadisticas import analizar_csv
+from estadisticas import estadisticas
+from utils import select_numeric, leer_csv
+from plots import (
+    plot_histograms,
+    plot_boxplots,
+    plot_scatter_matrix,)
+
 if __name__ == "__main__":
     archivo = input("Introduce la ruta del archivo CSV: ")
     # Validar la ruta del archivo
     if not os.path.isfile(archivo):
         print("Error: El archivo especificado no existe o no es un archivo válido.")
-    else:
-        analizar_csv(archivo)
+        exit(1)
+
+    df = leer_csv(archivo)
+    df_numericas = estadisticas(df)
+
+    # 2) Usar el DataFrame ya generado (se pasa entre funciones; no se relee)
+    df_num = select_numeric(df, df_numericas.columns.tolist())
+
+    # 3) Generar gráficas
+    plot_histograms(df_num, ".", bins=30, show=True)
+    plot_boxplots(df_num, ".", show=True)
+    plot_scatter_matrix(df_num, ".", show=True)
+
+    print(f"✅ Gráficas guardadas en el actual directorio.")
